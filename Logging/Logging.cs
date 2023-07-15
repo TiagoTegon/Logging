d5e4cf7370a715fafd86d47885bd6cbfdf34cc5a
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-15 11:11:09 -0300

    Adicionar arquivos de projeto.

---
+++ Logging/Logging.cs
@@ -0,0 +1,20 @@
+﻿public class Logging
+{
+    private static Logging? instance;
+
+    private Logging() { }
+
+    public static Logging Instance
+    {
+        get
+        {
+            instance ??= new Logging();
+            return instance;
+        }
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG] {message}");
+    }
+}
---
+++ Logging/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Logging logging = Logging.Instance;
+
+        logging.Log("This is a initial log message");
+    }
+}
